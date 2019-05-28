31337007639475905075ec6857a838a5b4d768a8
DFirsa <mr.danis2000@gmail.com>
2019-05-29 01:41:53 +0300

    Update controller

--- release/Test/src/sample/Controller.java
+++ release/Test/src/sample/Controller.java
@@ -1,10 +1,28 @@
 package sample;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.animation.FadeTransition;
+import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.Window;
+import javafx.util.Duration;
+
+import javax.swing.*;
 
 public class Controller {
 
@@ -32,10 +50,44 @@ public class Controller {
     @FXML
     private TextField firstMass;
 
+    private double firstLengthVal = 0;
+    private double secondLengthVal = 0;
+    private double firstMassVal = 0;
+    private double secondMassVal = 0;
+    private double startAngleVal = 0;
+
+    private boolean cathSmth = false;
+
     @FXML
     void initialize() {
         runButton.setOnAction(event -> {
-            System.out.println("get");
+            System.out.println("getValues: ");
+
+            try{
+
+                firstLengthVal = Double.valueOf(firstLength.getText());
+                secondLengthVal = Double.valueOf(secondLength.getText());
+                firstMassVal = Double.valueOf(firstMass.getText());
+                secondMassVal = Double.valueOf(secondMass.getText());
+                startAngleVal = Double.valueOf(startAngle.getText());
+
+            } catch (java.lang.NumberFormatException e){
+
+                cathSmth = true;
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+                alert.setTitle("Information");
+                alert.setHeaderText(null);
+                alert.setContentText("ENTER ALL INPUT PARAMETERS");
+
+                alert.showAndWait();
+            }
+
+            if (!cathSmth){
+
+            }
+
         });
     }
 }
